54a24f35d9abec877be7c33d4a526d2b1eabba82
Kayla Reopelle <kreopelle@gmail.com>
2018-04-08 18:14:21 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,8 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
-  
+  reversed_words = []
+  sentence_array.each do |word|
+    reversed_words << word.reverse
+  end
+  reversed_words.join(" ")
 end
